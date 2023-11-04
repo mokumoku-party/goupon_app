c09c9af167266a94d5706a9cb84243056294473a
dicenull <dicenull.dev@gmail.com>
2023-11-05 00:13:52 +0900

    🚿 デバッグ削除

--- lib/app.dart
+++ lib/app.dart
@@ -2,9 +2,8 @@ import 'package:app/routes/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-const primaryColor = Color(0xff7e7e7e);
 const backgroundColor = Color(0xfff1f1f1);
-const primaryColor = Color(0xff5e5e5e);
+const primaryColor = Color(0xff7e7e7e);
 const subSubColor = Color(0xffc9c9c9);
 const subTextColor = Color(0xff929292);
 const surfaceBorderColor = Color(0xffd9d9d9);

45cc64b97325f9f43bfe85cf7ad7abe5bbf49d1d
nabe1005 <taiki.w.1113@gmail.com>
2023-11-04 10:34:22 +0900

    ✨ StatefulShellRouteを利用して各タブを永続化できるようにした

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,12 +1,50 @@
 import 'package:app/pages/home_page.dart';
+import 'package:app/pages/scaffold_with_navbar.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final appRouterProvider = Provider(
   (ref) => GoRouter(
+    initialLocation: '/tab1',
     routes: [
+      StatefulShellRoute.indexedStack(
+        builder: (context, state, navigationShell) =>
+            ScaffoldWithNavbar(navigationShell),
+        branches: [
+          StatefulShellBranch(
+            routes: <RouteBase>[
+              GoRoute(
+                path: '/tab1',
+                builder: (context, state) {
+                  return const HomePage();
+                },
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: <RouteBase>[
+              GoRoute(
+                path: '/tab2',
+                builder: (context, state) {
+                  return const HomePage();
+                },
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: <RouteBase>[
+              GoRoute(
+                path: '/tab3',
+                builder: (context, state) {
+                  return const HomePage();
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
       GoRoute(
-        path: '/',
+        path: '/home',
         builder: (context, state) => const HomePage(),
       ),
     ],

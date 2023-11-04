b9bbfdd791c95220b6c6f9ab2c6e56cb2088d8de
dicenull <dicenull.dev@gmail.com>
2023-11-05 01:10:51 +0900

    👍 ぐーぽん追加

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -1,6 +1,11 @@
 import 'package:app/routes/app_router.dart';
+import 'package:dotlottie_loader/dotlottie_loader.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:lottie/lottie.dart';
+
+final completeGoupon = StateProvider((ref) => false);
 
 class ResultPage extends HookConsumerWidget {
   const ResultPage({super.key});
@@ -18,52 +23,90 @@ class ResultPage extends HookConsumerWidget {
           ),
         ),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            const Text('🎉🎉 当たり 🎉🎉'),
-            const Text('クーポンゲット！'),
-            Container(
-              decoration:
-                  BoxDecoration(borderRadius: BorderRadius.circular(40)),
-              child: Row(
-                children: [
-                  Container(
-                    width: 84,
-                    height: 84,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        image: AssetImage('assets/imgs/kani.png'),
-                      ),
+      body: ref.watch(completeGoupon) ? const _Result() : const _Goupon(),
+    );
+  }
+}
+
+class _Goupon extends HookConsumerWidget {
+  const _Goupon();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller = useAnimationController();
+
+    return Center(
+      child: Container(
+        color: Colors.white,
+        child: DotLottieLoader.fromAsset("assets/imgs/goupon.lottie",
+            frameBuilder: (BuildContext ctx, DotLottie? dotlottie) {
+          if (dotlottie != null) {
+            return Lottie.memory(dotlottie.animations.values.single,
+                controller: controller, onLoaded: (composition) {
+              controller
+                ..duration = composition.duration
+                ..forward().whenComplete(() {
+                  ref.read(completeGoupon.notifier).state = true;
+                });
+            });
+          } else {
+            return Container();
+          }
+        }),
+      ),
+    );
+  }
+}
+
+class _Result extends HookConsumerWidget {
+  const _Result();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      child: Column(
+        children: [
+          const Text('🎉🎉 当たり 🎉🎉'),
+          const Text('クーポンゲット！'),
+          Container(
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(40)),
+            child: Row(
+              children: [
+                Container(
+                  width: 84,
+                  height: 84,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage('assets/imgs/kani.png'),
                     ),
                   ),
-                  const Column(
-                    children: [
-                      Text('【クーポンの名前】'),
-                      Text('使える店名'),
-                      Text('有効期限'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF7E7E7E),
-                foregroundColor: Colors.white,
-                elevation: 0,
-                side: const BorderSide(
-                  color: Color(0xFF7E7E7E),
                 ),
-              ),
-              child: const Text('ホームに戻る'),
-              onPressed: () {
-                ref.read(appRouterProvider).go('/home');
-              },
+                const Column(
+                  children: [
+                    Text('【クーポンの名前】'),
+                    Text('使える店名'),
+                    Text('有効期限'),
+                  ],
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xFF7E7E7E),
+              foregroundColor: Colors.white,
+              elevation: 0,
+              side: const BorderSide(
+                color: Color(0xFF7E7E7E),
+              ),
+            ),
+            child: const Text('ホームに戻る'),
+            onPressed: () {
+              ref.read(appRouterProvider).go('/home');
+            },
+          ),
+        ],
       ),
     );
   }

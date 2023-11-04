5baa8cd085655d3a8509efda7f9a75862f7fd309
dicenull <dicenull.dev@gmail.com>
2023-11-04 16:30:16 +0900

    👍 デザイン調整

--- lib/pages/guide_page.dart
+++ lib/pages/guide_page.dart
@@ -1,5 +1,7 @@
 import 'package:app/app.dart';
+import 'package:app/routes/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class GuidePage extends HookConsumerWidget {
@@ -7,13 +9,6 @@ class GuidePage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final card = const SliverToBoxAdapter(
-      child: Padding(
-        padding: EdgeInsets.only(bottom: 16),
-        child: _ProfileCard(),
-      ),
-    );
-
     return Scaffold(
       appBar: AppBar(
         leading: const Icon(Icons.arrow_back_ios),
@@ -41,7 +36,20 @@ class GuidePage extends HookConsumerWidget {
                 ),
               ),
             ),
-            ...List.generate(10, (index) => card)
+            ...List.generate(
+              10,
+              (index) => SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 16),
+                  child: _ProfileCard(
+                    onTap: () {
+                      ref.read(appRouterProvider).go('/map');
+                    },
+                    medals: const ["a", "a", "a", "a", "a", "a"],
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
@@ -51,64 +59,84 @@ class GuidePage extends HookConsumerWidget {
 
 /// プロフィールカード
 class _ProfileCard extends HookConsumerWidget {
-final List<String> medals;
+  final List<String> medals;
+  final VoidCallback? onTap;
 
-  const _ProfileCard({required this.medals});
+  const _ProfileCard({required this.medals, this.onTap});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      padding: const EdgeInsets.all(20),
-      child: Row(
-        children: [
-          Container(
-            width: 84,
-            height: 84,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                image: AssetImage('assets/imgs/kani.png'),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        child: Row(
+          children: [
+            Container(
+              width: 84,
+              height: 84,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage('assets/imgs/kani.png'),
+                ),
               ),
             ),
-          ),
-          const SizedBox(width: 16),
-          const Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(children: [],)
-                Text(
-                  'ユーザー名最大１０字',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: textColor,
-                    fontWeight: FontWeight.bold,
+            const SizedBox(width: 4),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: medals
+                        .map((medal) =>
+                            SvgPicture.asset('assets/icons/medal.svg'))
+                        .toList(),
                   ),
-                ),
-                SizedBox(height: 4),
-                Text(
-                  'ふたつ名は最大１５文字ですよ！',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: textColor,
+                  const Padding(
+                    padding: EdgeInsets.only(left: 8),
+                    child: Text(
+                      'ユーザー名最大１０字',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: textColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                ),
-                SizedBox(height: 4),
-                Text(
-                  'ユーザーのプロフィール文とかあったほうがいいと思ったんです。2行くらい。',
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: subTextColor,
+                  const SizedBox(height: 4),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 8),
+                    child: Text(
+                      'ふたつ名は最大１５文字ですよ！',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: textColor,
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 4),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 8),
+                    child: Text(
+                      'ユーザーのプロフィール文とかあったほうがいいと思ったんです。2行くらい。あああああああああああああ',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: subTextColor,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
